1bf0651d77b2fd542bd1baa6679bc22a05138a93
Saranya Sarisa <20425@jhc.school.nz>
2025-03-24 13:29:51 +1300

    BAse code here
    still need to add a lot of code to suit my internal. Need to store data, idk how

--- main.py
+++ main.py
@@ -1,16 +1,79 @@
-# This is a sample Python script.
+'''Book Recommender'''
+# Date: 24/3/2025
+#                  Imports
+# used to create a custom window for age calculator
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import ttk
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# MAIN
+
+# creating custom window
+window = tk.Tk()
+window.geometry("300x300")
+window.config(bg="#F7DC6F")
+window.resizable(width=False, height=False)
+window.title('Book Recommender!')
+
+# Entry boxes for date, month and year
+e_genre = tk.Entry(window, width=5)
+
+# Labels for heading and subheading of GUI
+lb_heading = tk.Label(window, text="Book Recommender!", font=("Arial", 20), fg="black", bg="#F7DC6F")
+lb_subheading = tk.Label(window, font=("Arial", 12), text="Enter your favourite genre.",
+                         fg="black", bg="#F7DC6F")
+
+# Labels for date, month and year
+lb_genre = tk.Label(window, text="Genre: ", font=('Arial', 12, "bold"), fg="darkgreen", bg="#F7DC6F")
+
+# Label for text box that will display the calculated age
+lb_rbook = tk.Label(window, text="The recommended book for your is: ", font=('Arial', 12, "bold"), fg="darkgreen",
+                             bg="#F7DC6F")
+tbox_genre = tk.Text(window, width=5, height=0, state="disabled")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Functions
+def rec_book():
+    # get the three entries
+    g = (e_genre.get())
+
+  #  rec_book = randomise generator book
+    display_calc_age(calc_age) # display recommended book
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def find_age(d, m, y):
+    # find the age (difference between current and date of birth)
+    age = today.year - y - ((today.month, today.day) < (m, d))
+    tbox_age.config(state='normal')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def display_calc_age(age):
+    tbox_age.config(state='normal')
+    ##age calculated is dispayed in the text boz after clearning the previous info in the textbox
+    tbox_age.delete('1.0', tk.END)
+    tbox_age.insert(tk.END, age)
+    tbox_age.config(state='disabled')
+
+
+# Button to calculate age
+btn_calculate_age = tk.Button(window, text="Recommend Book!", font=("Arial", 13), command=rec_book)
+
+# Button to exit application
+btn_exit = tk.Button(window, text="Exit Application!", font=("Arial", 13), command=exit)
+
+
+def exit():
+    window.destroy()
+
+
+# Placing the elements on the screen
+lb_heading.place(x=70, y=5)
+lb_subheading.place(x=10, y=40)
+lb_genre.place(x=100, y=70)
+e_genre.place(x=180, y=70)
+btn_calculate_age.place(x=100, y=150)
+lb_rbook.place(x=50, y=200)
+tbox_genre.place(x=240, y=203)
+btn_exit.place(x=100, y=230)
+
+tk.mainloop()
