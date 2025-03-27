0f4a16fcff4cdf9e7367ab430e94075400d25019
Saranya Sarisa <20425@jhc.school.nz>
2025-03-27 13:14:49 +1300

    Add files via upload
    i only did the romance books so far. i will do the other genres and books at the end after I do everything else I need to do.

---
+++ Book Data.py
@@ -0,0 +1,52 @@
+#book data storage
+
+bttm = {
+    "name" : "Better than the Movies",
+    "author" : "Lynn Painter",
+    "ISBN" : "9781534467637",
+    "keywords" : "Y/A" "Young Adult" "Love" "Enemies to Lovers" "Fake Dating"
+}
+
+tbdate = {
+    "name" : "They Both Die at the End",
+    "author" : "Adam Silvera",
+    "ISBN" :"9780062457806",
+    "keywords" : "Dystopian" "LGBTQ+" "One Day" "Last Day" "Y/A"
+}
+
+tatbilb = {
+    "name": "To All the Boys I've Loved Before",
+    "author": "Jenny Han",
+    "ISBN": "9781442426719",
+    "keywords": "Love Letters" "Fake Dating" "Y/A"
+}
+
+hs = {
+    "name": "Heartstopper",
+    "author": "Alice Oseman",
+    "ISBN": "9781527225336",
+    "keywords": "LGBTQ+" "love" "teen romance" "Y/A"
+}
+
+tfios = {
+    "name": "The Fault in our Stars",
+    "author": "John Green",
+    "ISBN": "9780525478812",
+    "keywords":
+}
+
+romance = {
+    "bttm" : book1,
+    "tbdate" : book2,
+    "tatbilb" : book3,
+    "hs" : book4,
+    "tfios" : book5
+}
+
+
+
+
+"name": "",
+"author": "",
+"ISBN": "",
+"keywords":
