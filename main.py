9f886b178c13f84e4a48b6ee947450ad17765d52
Saranya Sarisa <20425@jhc.school.nz>
2025-03-31 13:39:48 +1300

    Add files via upload
    Got the book recomendations. next time, need to add a book in

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ window.config(bg="#F7DC6F")
 window.resizable(width=False, height=False)
 window.title('Book Recommender!')
 
-# Entry boxes for date, month and year
+# Entry boxes for fav genre
 e_genre = tk.Entry(window, width=5)
 
 # Labels for heading and subheading of GUI
@@ -34,7 +34,7 @@ lb_subheading = tk.Label(window, font=("Arial", 12), text="Enter your favourite 
 # Labels for date, month and year
 lb_genre = tk.Label(window, text="Genre: ", font=('Arial', 12, "bold"), fg="darkgreen", bg="#F7DC6F")
 
-# Label for text box that will display the calculated age
+# Label for text box that will display the book
 lb_rbook = tk.Label(window, text="The recommended book for your is: ", font=('Arial', 12, "bold"), fg="darkgreen",
                              bg="#F7DC6F")
 tbox_genre = tk.Text(window, width=5, height=0, state="disabled")
@@ -43,35 +43,44 @@ tbox_genre = tk.Text(window, width=5, height=0, state="disabled")
 # Functions
 def rec_book():
     # get the genre input
-    g = (e_genre.get())
-    if g = "romance":
+    g = e_genre.get()
+
+    #choose random book in genres
+    if g == "romance":
         rom = random.choice(list(book_data.romance.items()))
         print(rom)
+        display_rbook(rom)
 
-  #  rec_book = randomise generator book
+    elif g == "dystopian":
+        dys = random.choice(list(book_data.dystopian.items()))
+        print(dys)
 
-rom = random.choice(list(book_data.romance.items()))
-dys = random.choice(list(book_data.dystopian.items()))
+    def rbook():
+        rbook = rec_book
+        tbox_book.config(state='normal')
+
 
 #def find_book(g):
    #find book for reader
     #book = randomise genrator thingie
 
 
-def display_rec_book(book):
+def display_rbook(book):
     tbox_genre.config(state='normal')
     ##book info is dispayed in the text boz after clearning the previous info in the textbox
     tbox_genre.delete('1.0', tk.END)
-    tbox_genre.insert(tk.END, rec_book)
+    tbox_genre.insert(tk.END, book)
     tbox_genre.config(state='disabled')
 
 
-# Button to calculate age
-btn_calculate_age = tk.Button(window, text="Recommend Book!", font=("Arial", 13), command=rec_book)
+# Button to recomend book
+btn_recomend_book = tk.Button(window, text="Recommend Book!", font=("Arial", 13), command=rec_book)
 
 # Button to exit application
 btn_exit = tk.Button(window, text="Exit Application!", font=("Arial", 13), command=exit)
 
+#button to re-recomend book
+btn_read = tk.Button(window, text="Read Already", font=("Arial", 13), command=rec_book)
 
 def exit():
     window.destroy()
@@ -82,7 +91,8 @@ lb_heading.place(x=70, y=5)
 lb_subheading.place(x=10, y=40)
 lb_genre.place(x=100, y=70)
 e_genre.place(x=180, y=70)
-btn_calculate_age.place(x=100, y=150)
+btn_recomend_book.place(x=100, y=100)
+btn_read.place(x=120, y=160)
 lb_rbook.place(x=50, y=200)
 tbox_genre.place(x=240, y=203)
 btn_exit.place(x=100, y=230)
