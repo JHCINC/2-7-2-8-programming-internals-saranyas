f761a65121247e36a61a3e07b1211f393deb0bc9
Saranya Sarisa <20425@jhc.school.nz>
2025-04-08 10:50:09 +1200

    Add files via upload
    updated

--- book_data.py
+++ book_data.py
@@ -1,13 +1,13 @@
 #book data storage
 
-st = {
+Scythe = {
     "name": "Scythe",
     "author": "Neal Shusterman",
     "ISBN": "9781442472433",
     "keywords": "Y/A, Dystopian"
 }
 
-mr = {
+Maze_Runner = {
     "name": "Maze Runner",
     "author": "James Dashner",
     "ISBN": "9780385737944",
@@ -15,39 +15,39 @@ mr = {
 }
 
 dystopian = {
-    "st" : st,
-    "mr" : mr
+    "Scythe" : Scythe,
+    "Maze_Runner" : Maze_Runner
 }
 
-bttm = {
+Better_Than_the_Movies = {
     "name" : "Better than the Movies",
     "author" : "Lynn Painter",
     "ISBN" : "9781534467637",
     "keywords" : "Y/A, Young Adult, Love, Enemies to Lovers, Fake Dating"
 }
 
-tbdate = {
+They_Both_Die_at_the_End = {
     "name" : "They Both Die at the End",
     "author" : "Adam Silvera",
     "ISBN" :"9780062457806",
     "keywords" : "Dystopian, LGBTQ+, One Day, Last Day, Y/A"
 }
 
-tatbilb = {
+To_All_the_Boys_Ive_Loved_Before = {
     "name": "To All the Boys I've Loved Before",
     "author": "Jenny Han",
     "ISBN": "9781442426719",
     "keywords": "Love Letters, Fake Dating, Y/A"
 }
 
-hs = {
+Heartstopper = {
     "name": "Heartstopper",
     "author": "Alice Oseman",
     "ISBN": "9781527225336",
     "keywords": "LGBTQ+, love, teen romance, Y/A"
 }
 
-tfios = {
+The_Fault_in_Our_Stars = {
     "name": "The Fault in our Stars",
     "author": "John Green",
     "ISBN": "9780525478812",
@@ -55,11 +55,11 @@ tfios = {
 }
 
 romance = {
-    "bttm" : bttm,
-    "tbdate" : tbdate,
-    "tatbilb" : tatbilb,
-    "hs" : hs,
-    "tfios" : tfios
+    "Better_Than_the_Movies" : Better_Than_the_Movies,
+    "They_Both_Die_at_the_End" : They_Both_Die_at_the_End,
+    "To_All_the_Boys_Ive_Loved_Before" : To_All_the_Boys_Ive_Loved_Before,
+    "Heartstopper" : Heartstopper,
+    "The_Fault_in_Our_Stars" : The_Fault_in_Our_Stars
 }
 
 
