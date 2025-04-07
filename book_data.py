491c7c9b8e998fa3af756b70c8e706a5e714802b
Saranya Sarisa <20425@jhc.school.nz>
2025-04-07 13:20:34 +1200

    Add files via upload
    I cahnged the keywords

--- book_data.py
+++ book_data.py
@@ -4,14 +4,14 @@ st = {
     "name": "Scythe",
     "author": "Neal Shusterman",
     "ISBN": "9781442472433",
-    "keywords": "Y/A" "Dystopian"
+    "keywords": "Y/A, Dystopian"
 }
 
 mr = {
     "name": "Maze Runner",
     "author": "James Dashner",
     "ISBN": "9780385737944",
-    "keywords": "Y/a" "Friendship"
+    "keywords": "Y/a, Friendship"
 }
 
 dystopian = {
@@ -23,28 +23,28 @@ bttm = {
     "name" : "Better than the Movies",
     "author" : "Lynn Painter",
     "ISBN" : "9781534467637",
-    "keywords" : "Y/A" "Young Adult" "Love" "Enemies to Lovers" "Fake Dating"
+    "keywords" : "Y/A, Young Adult, Love, Enemies to Lovers, Fake Dating"
 }
 
 tbdate = {
     "name" : "They Both Die at the End",
     "author" : "Adam Silvera",
     "ISBN" :"9780062457806",
-    "keywords" : "Dystopian" "LGBTQ+" "One Day" "Last Day" "Y/A"
+    "keywords" : "Dystopian, LGBTQ+, One Day, Last Day, Y/A"
 }
 
 tatbilb = {
     "name": "To All the Boys I've Loved Before",
     "author": "Jenny Han",
     "ISBN": "9781442426719",
-    "keywords": "Love Letters" "Fake Dating" "Y/A"
+    "keywords": "Love Letters, Fake Dating, Y/A"
 }
 
 hs = {
     "name": "Heartstopper",
     "author": "Alice Oseman",
     "ISBN": "9781527225336",
-    "keywords": "LGBTQ+" "love" "teen romance" "Y/A"
+    "keywords": "LGBTQ+, love, teen romance, Y/A"
 }
 
 tfios = {
